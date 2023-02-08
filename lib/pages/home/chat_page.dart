492e96a46099a38f302b34b429ebe5ffb27f986d
hafizhsdq <hafizhassidiq77@gmail.com>
2023-02-08 11:03:50 +0700

    main page

---
+++ lib/pages/home/chat_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("chat page", style: primaryTextStyle,),
+    );
+  }
+}
