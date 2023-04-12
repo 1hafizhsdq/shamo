1157144636b8fd0d0d4b92a1381c0465d88398c8
dev kominfosda <dev.kominfosda@gmail.com>
2023-04-12 11:07:26 +0700

    send chat firestore

---
+++ lib/models/message_model.dart
@@ -0,0 +1,49 @@
+import 'package:shamo/models/product_model.dart';
+
+class MessageModel {
+  String message;
+  int userId;
+  String userName;
+  String userImage;
+  bool isFromUser;
+  ProductModel product;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  MessageModel({
+    required this.message,
+    required this.userId,
+    required this.userName,
+    required this.userImage,
+    required this.isFromUser,
+    required this.product,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory MessageModel.fromJson(Map<String, dynamic> json) {
+    return MessageModel(
+      message: json['message'],
+      userId: json['userId'],
+      userName: json['userName'],
+      userImage: json['userImage'],
+      isFromUser: json['isFromUser'],
+      product: json['product'] == {} ? UninitializedProductModel() : ProductModel.fromJson(json['product']),
+      createdAt: DateTime.parse(json['created_at']),
+      updatedAt: DateTime.parse(json['updated_at']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'message': message,
+      'userId': userId,
+      'userName': userName,
+      'userImage': userImage,
+      'isFromUser': isFromUser,
+      'product': product is UninitializedProductModel ? {} : product.toJson(),
+      'created_at': createdAt.toString(),
+      'updated_at': updatedAt.toString(),
+    };
+  }
+}
